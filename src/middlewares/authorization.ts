528468c601c96df9f76412e4d22406aaab7c0119
QHuyyyyy <huy120904@gmail.com>
2025-09-22 15:19:29 +0700

    add type fix

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -1,32 +1,32 @@
-const authorize = (roles = []) => {
-    return (req, res, next) => {
-        if (!req.user) {
-            return res.status(401).json({ message: 'Unauthorized: No user information' });
-        }
+// const authorize = (roles = []) => {
+//     return (req, res, next) => {
+//         if (!req.user) {
+//             return res.status(401).json({ message: 'Unauthorized: No user information' });
+//         }
 
-        if (typeof roles === 'string') {
-            roles = [roles];
-        }
+//         if (typeof roles === 'string') {
+//             roles = [roles];
+//         }
 
-        if (!roles.includes(req.user.role)) {
-            return res.status(403).json({
-                message: 'Forbidden: Insufficient permissions',
-                required: roles,
-                current: req.user.role
-            });
-        }
+//         if (!roles.includes(req.user.role)) {
+//             return res.status(403).json({
+//                 message: 'Forbidden: Insufficient permissions',
+//                 required: roles,
+//                 current: req.user.role
+//             });
+//         }
 
-        next();
-    };
-};
+//         next();
+//     };
+// };
 
-// Specific authorization middlewares
-const authorizeAdmin = authorize(['ADMIN']);
-const authorizeStaff = authorize(['ADMIN', 'STAFF']);
-const authorizeTechnician = authorize(['ADMIN', 'TECHNICIAN']);
-module.exports = {
-    authorize,
-    authorizeAdmin,
-    authorizeStaff,
-    authorizeTechnician
-};
+// // Specific authorization middlewares
+// const authorizeAdmin = authorize(['ADMIN']);
+// const authorizeStaff = authorize(['ADMIN', 'STAFF']);
+// const authorizeTechnician = authorize(['ADMIN', 'TECHNICIAN']);
+// module.exports = {
+//     authorize,
+//     authorizeAdmin,
+//     authorizeStaff,
+//     authorizeTechnician
+// };
